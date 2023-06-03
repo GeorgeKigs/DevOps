80cc19ab244e8168cac707a77cce8c1f9fb72dd5
GeorgeKigs <gkndungu99@gmail.com>
2023-06-03 20:47:24 +0300

    commenting out the gradle installation

--- script.sh
+++ script.sh
@@ -4,21 +4,21 @@
 # required within Docker to run the script successfully
 
 # Check if unzip is installed and install it if necessary
-if ! command -v unzip &>/dev/null; then
-    echo "unzip is not installed. Installing unzip..."
-    sudo apt-get update
-    sudo apt-get install -y unzip
-fi
+# if ! command -v unzip &>/dev/null; then
+#     echo "unzip is not installed. Installing unzip..."
+#     sudo apt-get update
+#     sudo apt-get install -y unzip
+# fi
 
 # deleting any file with gradle
 
 # rm -rf /opt/gradle/gradle-8.1.1
 
 # Install Gradle
-if ! [ -d /opt/gradle/gradle-8.1.1-bin/ ]; then
-wget https://services.gradle.org/distributions/gradle-8.1.1-bin.zip -P /tmp
-sudo unzip -d /opt/gradle /tmp/gradle-8.1.1-bin.zip
-fi
+# if ! [ -d /opt/gradle/gradle-8.1.1-bin/ ]; then
+# wget https://services.gradle.org/distributions/gradle-8.1.1-bin.zip -P /tmp
+# sudo unzip -d /opt/gradle /tmp/gradle-8.1.1-bin.zip
+# fi
 echo "export PATH=/opt/gradle/gradle-8.1.1/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
 
