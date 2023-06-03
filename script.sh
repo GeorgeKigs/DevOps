a2b60cc24c4f098228cc94e73d6dbc8bb994b0fa
GeorgeKigs <gkndungu99@gmail.com>
2023-06-03 22:49:43 +0300

    commiting changes to the script file

--- script.sh
+++ script.sh
@@ -15,14 +15,14 @@
 # rm -rf /opt/gradle/gradle-8.1.1
 
 # Install Gradle
-if ! [ -d /opt/gradle/gradle-8.1.1-bin/ ]; then
+if ! [ -d /opt/gradle/gradle-8.1.1/bin/ ]; then
 wget https://services.gradle.org/distributions/gradle-8.1.1-bin.zip -P /tmp
 sudo unzip -d /opt/gradle /tmp/gradle-8.1.1-bin.zip
 fi
-echo "export PATH=/opt/gradle/gradle-8.1.1/bin:\$PATH" >> ~/.bashrc
-echo $PATH
-source ~/.bashrc
+export PATH="/opt/gradle/gradle-8.1.1/bin:$PATH" >> ~/.bashrc
 
+source ~/.bashrc
+echo $PATH
 # Add jenkins user to docker group
 # sudo usermod -aG docker jenkins
 
